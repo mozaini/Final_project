bae9db7a17d55a156a2d55e1f3d227e87b015a0d
Abdullah Almuzaini <almuzaini224@gmail.com>
2021-09-25 16:19:41 -0400

    remove arguments (id_var and value_vars)

--- reshape_dataset.py
+++ reshape_dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def reshape(data, id_vars, value_vars, var_name, value_name):
+def reshape(data, var_name, value_name):
     pd.melt(data, id_vars=data.columns[:4],
             value_vars=data.columns[4:],
             var_name=var_name,
