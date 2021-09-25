faed712c40d9a1dfd592e1c5c7555a657aca3e0f
Abdullah Almuzaini <almuzaini224@gmail.com>
2021-09-25 12:26:49 -0400

    create a script to fetch the dataset from its source

---
+++ get_dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import requests as re
+import os
+def get_dataset(url, path):
+    """
+        The following function will download the dataset from the internet using requests library and
+        save it in the 'dataset' directory inside the current running direcroty.
+
+        The function will take url and the file name including its format `csv` for example
+
+        """
+
+    response = re.get(url)
+    with open(os.path.join('dataset', url.split('/')[-1]), mode='wb') as file:
+        file.write(response.content)
